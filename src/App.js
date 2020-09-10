c0b7000d3e68875cfc0de640f00d7c9fda5d4d73
Francisco Suero <suerofrancisco@yahoo.com>
2020-09-10 17:03:32 -0400

    start

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '',
+                  transition: false};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+  const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({user: { name: this.state.name }})
+  };
+  fetch('http://localhost:3000/users/', requestOptions)
+      .then(response => response.json())
+      .then(this.state.transition = true)
+      event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-export default App;
+export default NameForm;
